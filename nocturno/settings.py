52f82927b10bcfa930a6a045ef5386293dd165db
Daniel Torres <daniel.torres@xaldigital.com>
2023-08-23 09:04:47 -0600

    deploy con controlador postgres

--- nocturno/settings.py
+++ nocturno/settings.py
@@ -23,7 +23,7 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, 'secure/.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -163,7 +163,7 @@ CONFIG_DATA_AWS = {
     "query": "",
 }
 
-ssh_key_pem = os.path.join(BASE_DIR, 'palaciokey.pem')
+ssh_key_pem = os.path.join(BASE_DIR, 'secure/palaciokey.pem')
 
 ssh_server = "ec2-18-213-16-147.compute-1.amazonaws.com"
 
